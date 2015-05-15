8ef5fa54fd317f60d2d8b790dfa00b37eb36ba05
Sebastian Sandqvist <seb@sparque.me>
2015-05-14 18:49:07 -0700

    added tests and cleaner code

--- index.js
+++ index.js
@@ -1,23 +1,77 @@
 'use strict';
 
-module.exports = function(lat1, lon1, lat2, lon2) {
+var haversine = module.exports = {};
 
-	var degreesToRadians = function (deg) {
-		return deg * (Math.PI/180);
-	};
 
-	var earthRadius = 6371; // Radius of the earth in km
-	
-	var latitudeDifference = degreesToRadians(lat2 - lat1);
-	var logitudeDifference = degreesToRadians(lon2 - lon1);
+// ----- configurable earthRadius
+// ---------------------------------------
+haversine.earthRadius = 6371000;
 
-	var haversine = 
+
+// ----- convert degrees to radians
+// ---------------------------------------
+haversine.degreesToRadians = function(deg) {
+	return deg * (Math.PI / 180);
+};
+
+
+// ----- distance between two points
+// ---------------------------------------
+haversine.distance = function(lat1, lon1, lat2, lon2) {
+
+	var latitudeDifference = this.degreesToRadians(lat2 - lat1);
+	var logitudeDifference = this.degreesToRadians(lon2 - lon1);
+
+	var haversine =
 		Math.sin(latitudeDifference/2) * Math.sin(latitudeDifference/2) +
-		Math.cos(degreesToRadians(lat1)) * Math.cos(degreesToRadians(lat2)) * 
+		Math.cos(this.degreesToRadians(lat1)) * Math.cos(this.degreesToRadians(lat2)) * 
 		Math.sin(logitudeDifference/2) * Math.sin(logitudeDifference/2); 
-	
-	var distanceInRadians = 2 * Math.atan2(Math.sqrt(haversine), Math.sqrt(1-haversine)); 
 
-	return earthRadius * distanceInRadians; // Distance in km
+	var distance = 2 * Math.atan2(Math.sqrt(haversine), Math.sqrt(1 - haversine));
+
+	return this.earthRadius * distance;
+
+};
+
+
+// ----- convert degrees/minutes/seconds to decimal degrees
+// ---------------------------------------
+haversine.toDecimal = function(str) {
+
+	str = str.toLowerCase(); // throws if input is not a string
+	var lastChar = str.slice(-1);
+	var negative = false;
+
+	if (lastChar === 'w' || lastChar === 's') {
+		negative = true; // south and west => negative
+	}
+
+	var values = str.split(/[^0-9.]/);
+	var i;
+	// convert strings to numbers
+	for (i = 0; i < values.length; i++) {
+
+		// remove empty values
+		if (!values[i]) {
+			values.splice(i, 1);
+			continue;
+		}
+
+		values[i] = parseFloat(values[i]);
+
+	}
+
+	// make sure array has length 3
+	for (i = 0; i < 3; i++) {
+		values[i] = values[i] || 0;
+	}
+
+	var result = values[0] + (values[1] / 60) + (values[2] / 3600);
+
+	if (negative) {
+		return result * -1;
+	}
+
+	return result;
 
 };
--- test/spec.js
+++ test/spec.js
@@ -7,8 +7,43 @@ require('blanket')({
 });
 
 var expect = require('chai').expect;
-var app = require('../index.js');
+var haversine = require('../index.js');
 
 
 // ----- tests
 // ---------------------------------------
+describe('haversine', function() {
+
+	it('should have correct earth radius', function() {
+		expect(haversine.earthRadius).to.equal(6371000);
+	});
+
+	it('should get distance between two points', function() {
+		expect(haversine.distance(40, 50, 40, 50)).to.equal(0);
+		expect(haversine.distance(40, -70, -70, 40)).to.be.closeTo(14890000, 5000);
+		expect(haversine.distance(36.151829, -115.143296, 36.151465, -115.146284)).to.be.closeTo(300, 100);
+	});
+
+});
+
+describe('toDecimal', function() {
+	it('should convert degrees minutes seconds to decimal', function() {
+		expect(haversine.toDecimal('40° N')).to.equal(40);
+		expect(haversine.toDecimal('40.1° N')).to.equal(40.1);
+		expect(haversine.toDecimal('40 20')).to.equal(40.333333333333336);
+		expect(haversine.toDecimal('40 20 0')).to.equal(40.333333333333336);
+		expect(haversine.toDecimal('40 20 50')).to.equal(40.34722222222222);
+		expect(haversine.toDecimal('40 20 50N')).to.equal(40.34722222222222);
+		expect(haversine.toDecimal('40 20 50W')).to.equal(-40.34722222222222);
+		expect(haversine.toDecimal('40°20\'50" S')).to.equal(-40.34722222222222);
+	});
+});
+
+describe('degrees to radians', function() {
+	it('should convert degrees to radians', function() {
+		expect(haversine.degreesToRadians(0)).to.equal(0);
+		expect(haversine.degreesToRadians(15)).to.equal(Math.PI / 12);
+		expect(haversine.degreesToRadians(180)).to.equal(Math.PI);
+		expect(haversine.degreesToRadians(360)).to.equal(Math.PI * 2);
+	});
+});
